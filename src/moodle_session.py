7a3494bb71ef315845380a8e2532399541b32733
Callow <stroganov-oleges@yandex.ru>
2021-03-29 14:15:53 +0300

    add async process

--- src/moodle_session.py
+++ src/moodle_session.py
@@ -1,7 +1,6 @@
-import urllib3
-import requests
 import re
 import json
+import aiohttp
 from datetime import datetime
 
 from src.calendar_manager import CalendarManager
@@ -12,81 +11,68 @@ class MoodleSession:
     __sessions = {}
 
     @staticmethod
-    def get_session(username):
+    async def get_session(username):
         if username not in MoodleSession.__sessions:
-            MoodleSession.__sessions[username] = MoodleSession(username)
+            MoodleSession.__sessions[username] = MoodleSession(username,
+                                                               aiohttp.ClientSession(requote_redirect_url=False))
         return MoodleSession.__sessions[username]
 
-    def __init__(self, username):
+    def __init__(self, username, session: aiohttp.ClientSession):
         self.calendar = CalendarManager()
         self.session_key = None
         self.username = username
         self.password = None
-        self.session = requests.Session()
+        self.session = session
 
     def update_password(self, password: str):
         self.password = password
 
-    def is_logged_in(self) -> bool:
+    async def is_logged_in(self) -> bool:
         if self.session_key is not None:
             url = "https://do.sevsu.ru/my/"
-            main_page_response = self.session.get(url)
-            return url == main_page_response.url
+            main_page_response = await self.session.get(url)
+            return url == main_page_response.url.__str__()
         return False
 
-    def login(self, password: str = None):
+    async def login(self, password: str = None):
         if password is None:
             if self.password is None:
                 raise Exception(
                     "You must to determine password in 'login' function or with 'update_password' function.")
             else:
                 password = self.password
-        # if debug:
-        #     proxies = {
-        #         'http': 'http://127.0.0.1:8080',
-        #         'https': 'http://127.0.0.1:8080',
-        #     }
-        #     session.proxies.update(proxies)
-        #     session.headers.update(
-        #         {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0"})
-        #     session.verify = False
-        #     urllib3.disable_warnings()
 
-        auth_site = self.session.get("https://do.sevsu.ru/auth/saml2/login.php",
-                                     params={"wants": "https://do.sevsu.ru/?",
-                                             "idp": "64044bc53c749f0a74e4a4f13b1c0884",
-                                             "passive": "off"})
+        to_auth_params = {"wants": "https://do.sevsu.ru/?", "idp": "64044bc53c749f0a74e4a4f13b1c0884",
+                          "passive": "off"}
+        async with self.session.get("https://do.sevsu.ru/auth/saml2/login.php",
+                                    params=to_auth_params, allow_redirects=True) as to_auth_resp:
+            auth_link_regex = re.compile(
+                r"<form id=\"login-form\" onsubmit=\"login.disabled = true; return true;\" action=\"([^\"]+)\"")
+            auth_link = re.search(auth_link_regex, await to_auth_resp.text()).group(1)
+            auth_link = auth_link.replace("&amp;", "&")  # Replacing url encoded "&"
 
-        auth_link_regex = re.compile(
-            r"<form id=\"login-form\" onsubmit=\"login.disabled = true; return true;\" action=\"([^\"]+)\"")
-        auth_link = re.search(auth_link_regex, auth_site.text).group(1)
-        auth_link = auth_link.replace("&amp;", "&")  # Replacing url encoded "&"
         login_data = {"username": self.username,
                       "password": password,
                       "rememberMe": "on", "credentialId": ""}
+        async with self.session.post(auth_link, data=login_data) as login_response:
+            login_response_text = await login_response.text()
+            if re.search(auth_link_regex, login_response_text):
+                raise ValueError(f"Incorrect username or password. -> {self.username}")
+            print(f"Successfully logged in. -> {self.username}")
 
-        login_response_content = self.session.post(auth_link, data=login_data).text
-        if re.search(auth_link_regex, login_response_content):
-            print(f"Incorrect username or password. -> {self.username}")
-            return
-
-        print(f"Successfully logged in. -> {self.username}")
         saml_login_link_regex = re.compile(r"<form name=\"saml-post-binding\" method=\"post\" action=\"([^\"]+)\"")
         saml_login_data_regex = re.compile(r"<input type=\"hidden\" name=\"(\w+)\" value=\"([^\"]+)\"")
-
-        saml_login_link = saml_login_link_regex.search(login_response_content).group(1)
+        saml_login_link = saml_login_link_regex.search(login_response_text).group(1)
         saml_login_data = {}
-        for match in saml_login_data_regex.finditer(login_response_content):
+        for match in saml_login_data_regex.finditer(login_response_text):
             saml_login_data[match.group(1)] = match.group(2)
 
-        main_page_content = self.session.post(saml_login_link, data=saml_login_data).content.decode()
+        async with self.session.post(saml_login_link, data=saml_login_data) as main_page_response:
+            session_key_regex = re.compile(r"\"sesskey\":\"([^\"]+)\"")
+            self.session_key = session_key_regex.search(await main_page_response.text()).group(1)
 
-        session_key_regex = re.compile(r"\"sesskey\":\"([^\"]+)\"")
-
-        self.session_key = session_key_regex.search(main_page_content).group(1)
-
-    def update_calendar(self):
-        if not self.is_logged_in():
+    async def update_calendar(self):
+        if not (await self.is_logged_in()):
             raise Exception("You must login before update_calendar.")
 
         # self.calendar.clear()
@@ -100,15 +86,24 @@ class MoodleSession:
             }
         }
         request_data = json.dumps([request_dict])
-        calendar_response = self.session.post("https://do.sevsu.ru/lib/ajax/service.php",
-                                              params={"sesskey": self.session_key, "info": method_name},
-                                              data=request_data)
+        async with self.session.post("https://do.sevsu.ru/lib/ajax/service.php",
+                                     params={"sesskey": self.session_key, "info": method_name},
+                                     data=request_data) as calendar_response:
 
-        day_data = calendar_response.json()[0]
+            day_data = (await calendar_response.json())[0]
+
         if day_data["error"]:
-            raise Exception("Have got error in calendar day info response.")
+            raise ValueError("Have got error in calendar day info response.")
 
         day_events = day_data["data"]["events"]
         for event in day_events:
             if event["eventtype"] == "attendance":
                 self.calendar.add_event(event["timestart"], event["timeduration"], event["url"])
+
+    async def mark_available_attendance(self):
+        active_links = self.calendar.get_active_events()
+        for link in active_links:
+            pass
+
+    async def close(self):
+        await self.session.close()
--- tests/session_test.py
+++ tests/session_test.py
@@ -1,18 +1,37 @@
+import asyncio
 from src.moodle_session import MoodleSession
 
-session: MoodleSession = MoodleSession.get_session("hello")
-session.update_password("zero")
-assert not session.is_logged_in(), "is_logged_in test without login failed"
 
-session.login()
-assert not session.is_logged_in(), "is_logged_in test with login failed"
+async def main():
+    tasks = [asyncio.create_task(test_1()), asyncio.create_task(test_2())]
+    await asyncio.gather(*tasks)
+    print("All tests was passed.")
 
-session = MoodleSession.get_session("00-010670")
-session.update_password("WDD7tves")
 
-session.login()
-assert session.is_logged_in(), "login with correct credentials failed"
+async def test_1():
+    session: MoodleSession = await MoodleSession.get_session("hello")
+    session.update_password("zero")
+    assert not (await session.is_logged_in()), "is_logged_in test without login failed"
 
-session.update_calendar()
+    try:
+        await session.login()
+    except ValueError:
+        print("incorrect login test passed")
+    else:
+        assert False, "incorrect login test failed"
+    await session.close()
 
-print("All tests was passed.")
+
+async def test_2():
+    session = await MoodleSession.get_session("00-010670")
+    session.update_password("WDD7tves")
+
+    await session.login()
+    result = await session.is_logged_in()
+    assert result, "login with correct credentials failed"
+
+    await session.update_calendar()
+    await session.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
