2ebac934c0857261ab32275f8dd56e63fde4b98c
Callow <stroganov-oleges@yandex.ru>
2021-03-30 13:01:44 +0300

    refactor command struct

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import asyncio
+from src.moodle_session import MoodleSession
 import src.session_manager as session_manager
 import src.data_manager as data_manager
 import aioconsole
 
 
 async def terminal_loop():
-    print_command_list()
+    print_command_list(None)
     while True:
         command_args = (await aioconsole.ainput("> ")).split()
         if len(command_args) == 0:
@@ -13,46 +14,78 @@ async def terminal_loop():
 
         command = command_args[0]
         args = command_args[1:]
-        if command == "user_add":
-            if len(args) < 2:
-                print("To few arguments.")
-                continue
-            data_manager.add_user(args[0], args[1])
-        elif command == "user_remove":
-            if len(args) < 1:
-                print("To few arguments.")
-                continue
-            data_manager.remove_user(args[0])
-        elif command == "user_list":
-            for username, password in data_manager.get_users():
-                print(f"{username}:{password}")
-        elif command == "pref_change":
-            if len(args) < 2:
-                print("To few arguments.")
-                continue
-            data_manager.change_preference(args[0], args[1])
-        elif command == "pref_list":
-            for pref_name, pref_value in data_manager.get_preferences()["DEFAULT"].items():
-                print(pref_name + " = " + pref_value)
-        elif command == "command_list":
-            print_command_list()
+        for command_name, command_info in command_list.items():
+            if command_name == command:
+                if len(args) < command_info["args"]:
+                    print("Too few arguments.")
+                    break
+                _ = command_info["func"](args)
+                break
         else:
             print("Unknown command:", command)
 
 
-def print_command_list():
-    print("Commands:")
-    print("\tuser_add <name> <password>")
-    print("\tuser_remove <name>")
-    print("\tuser_list")
-    print("\tpref_change <name> <value>")
-    print("\tpref_list")
-    print("\tcommand_list")
+def print_command_list(_):
+    for command_name, command_info in command_list.items():
+        print(command_name, command_info["usage"])
+
+
+def print_user_list(_):
+    for username, password in data_manager.get_users():
+        print(f"{username}:{password}")
+
+
+def print_prefs_list(_):
+    for pref_name, pref_value in data_manager.get_preferences()["DEFAULT"].items():
+        print(pref_name + " = " + pref_value)
 
 
 async def main():
     await asyncio.gather(session_manager.session_loop(), terminal_loop())
 
+command_list = {
+    "user_add": {
+        "usage": "<name> <password>",
+        "args": 2,
+        "func": lambda args: data_manager.add_user(args[0], args[1])
+    },
+
+    "user_get_calendar": {
+        "usage": "<name>",
+        "args": 1,
+        "func": lambda args: MoodleSession.get_session(args[0]).calendar.show()
+    },
+
+    "user_remove": {
+        "usage": "<name>",
+        "args": 1,
+        "func": lambda args: data_manager.remove_user(args[0])
+    },
+
+    "user_list": {
+        "usage": "",
+        "args": 0,
+        "func": print_user_list
+    },
+
+    "pref_change": {
+        "usage": "<name> <value>",
+        "args": 2,
+        "func": lambda args: data_manager.change_preference(args[0], args[1])
+    },
+
+    "pref_list": {
+        "usage": "",
+        "args": 0,
+        "func": print_prefs_list
+    },
+
+    "command_list": {
+        "usage": "<name> <password>",
+        "args": 0,
+        "func": print_command_list
+    }
+}
 
 if __name__ == "__main__":
     asyncio.run(main())
